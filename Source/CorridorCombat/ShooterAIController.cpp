9648a48eec2b5d50befd7b868ab57533683e84fe
Jonathan Moallem <jdmoallem@gmail.com>
2020-08-07 17:57:43 +1000

    Added basic AI controller class

---
+++ Source/CorridorCombat/ShooterAIController.cpp
@@ -0,0 +1,46 @@
+// Jonathan Moallem 2020
+
+
+#include "ShooterAIController.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/World.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+
+void AShooterAIController::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if (AIBehaviour)
+    {
+        RunBehaviorTree(AIBehaviour); // WARNING: MUST SET THIS BEFORE ACCESSING THE BLACKBOARD
+        BlackboardComponent = GetBlackboardComponent();
+    }
+
+    Player = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
+    APawn* AIPawn = GetPawn();
+    if (AIPawn)
+    {
+        BlackboardComponent->SetValueAsVector(TEXT("StartLocation"), AIPawn->GetActorLocation());
+    }
+}
+
+
+void AShooterAIController::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    if (!Player) { return; }
+
+    if (LineOfSightTo(Player))
+    {
+        BlackboardComponent->SetValueAsVector(TEXT("PlayerLocation"), Player->GetActorLocation());
+        BlackboardComponent->SetValueAsVector(TEXT("LastKnownPlayerLocation"), Player->GetActorLocation());
+    }
+    else
+    {
+        BlackboardComponent->ClearValue(TEXT("PlayerLocation"));
+    }
+
+}
---
+++ Source/CorridorCombat/ShooterAIController.h
@@ -0,0 +1,43 @@
+// Jonathan Moallem 2020
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "ShooterAIController.generated.h"
+
+
+UCLASS()
+class CORRIDORCOMBAT_API AShooterAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+
+    // Constructor & public default methods
+
+    virtual void Tick(float DeltaTime) override;
+
+protected:
+
+    // Protected default methods
+
+    virtual void BeginPlay() override;
+
+private:
+
+    // Component fields
+
+    UPROPERTY()
+    UBlackboardComponent* BlackboardComponent;
+
+    // Behaviour fields
+
+    UPROPERTY(EditAnywhere, Category = "Behaviour")
+    UBehaviorTree* AIBehaviour;
+
+    // Other fields
+
+    APawn* Player;
+	
+};
